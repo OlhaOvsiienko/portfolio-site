33a50ff68e499332642c36638eb5733771ab0922
OlhaOvsiienko <o.v.ovsiyenko@gmail.com>
2024-07-16 18:37:32 +0300

    Change Header (add avatar to mobile, relocate menu to the right on mobile)

--- src/components/header-title/HeaderTitle.jsx
+++ src/components/header-title/HeaderTitle.jsx
@@ -1,27 +1,19 @@
 import './HeaderTitle.styles.css'
-import useDeviceType from '../../hooks/use-device-type.jsx'
 import Avatar from '../../images/avatar.png'
 
 const HeaderTitle = () => {
-    const { isTablet, isDesktop } = useDeviceType()
+    
   
     return (
       <div className="header-title-container">
-        
-        {(isDesktop || isTablet) && (
-            <div className="header-avatar">
-                <img src={Avatar} alt="Avatar" className="avatar" />
-            </div>
-         
-        )}
-
+        <div className="header-avatar">
+          <img src={Avatar} alt="Avatar" className="avatar" />
+        </div>
         <h4 className="header-text">
             Olha Ovsiienko 
             <br />
             Trainee FrontEnd Developer
         </h4>
-            
-        
       </div>
     )
   }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,17 @@
-import React from 'react'
-import './Header.styles.css'
-import useDeviceType from '../../hooks/use-device-type.jsx'
 import Menu from '../menu/Menu.jsx'
 import HeaderTitle from '../header-title/HeaderTitle.jsx'
-
+import './Header.styles.css'
 
 const Header = () => {
-  const { isMobile, isTablet, isDesktop } = useDeviceType()
 
   return (
     <header className="header-container">
-      {(isMobile) && (
-        <>
-          <Menu />
-          <HeaderTitle />
-        </>
-      )}
-      {(isDesktop || isTablet) && (
         <>
           <HeaderTitle />
           <Menu />
         </>
-      )}
     </header>
-  );
-};
+  )
+}
 
-export default Header  
+export default Header 
