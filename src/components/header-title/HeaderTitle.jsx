00ee54e898e337e6aed09f38e7de91f584fb1267
OlhaOvsiienko <o.v.ovsiyenko@gmail.com>
2024-07-16 18:50:27 +0300

    add variant (in comments) without avatar on mobile

--- src/components/header-title/HeaderTitle.jsx
+++ src/components/header-title/HeaderTitle.jsx
@@ -1,4 +1,34 @@
 import './HeaderTitle.styles.css'
+import useDeviceType from '../../hooks/use-device-type.jsx'
+import Avatar from '../../images/avatar.png'
+
+const HeaderTitle = () => {
+    const { isTablet, isDesktop } = useDeviceType()
+  
+    return (
+      <div className="header-title-container">
+        
+        {(isDesktop || isTablet) && (
+            <div className="header-avatar">
+                <img src={Avatar} alt="Avatar" className="avatar" />
+            </div>
+         
+        )}
+
+        <h4 className="header-text">
+            Olha Ovsiienko 
+            <br />
+            Trainee FrontEnd Developer
+        </h4>
+            
+        
+      </div>
+    )
+  }
+  
+  export default HeaderTitle  
+
+/*import './HeaderTitle.styles.css'
 import Avatar from '../../images/avatar.png'
 
 const HeaderTitle = () => {
@@ -18,4 +48,4 @@ const HeaderTitle = () => {
     )
   }
   
-  export default HeaderTitle
+  export default HeaderTitle  */
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,35 @@
+import React from 'react'
+import './Header.styles.css'
+import useDeviceType from '../../hooks/use-device-type.jsx'
 import Menu from '../menu/Menu.jsx'
+import HeaderTitle from '../header-title/HeaderTitle.jsx'
+
+
+const Header = () => {
+  const { isMobile, isTablet, isDesktop } = useDeviceType()
+
+  return (
+    <header className="header-container">
+      {(isMobile) && (
+        <>
+          <Menu />
+          <HeaderTitle />
+        </>
+      )}
+      {(isDesktop || isTablet) && (
+        <>
+          <HeaderTitle />
+          <Menu />
+        </>
+      )}
+    </header>
+  );
+};
+
+export default Header  
+
+
+/*import Menu from '../menu/Menu.jsx'
 import HeaderTitle from '../header-title/HeaderTitle.jsx'
 import './Header.styles.css'
 
@@ -14,4 +45,4 @@ const Header = () => {
   )
 }
 
-export default Header 
+export default Header */
